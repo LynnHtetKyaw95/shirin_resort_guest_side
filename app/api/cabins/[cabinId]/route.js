440a2dd12931541b2d6cb44977f863909e7561b0
lhk <dklin1995@gmail.com>
2026-03-26 13:54:19 +0700

    API handle routes

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,16 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/apiService";
+
+export async function GET(req, { params }) {
+  const { cabinId } = await params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+
+    return Response.json({ cabin, bookedDates });
+  } catch (error) {
+    return Response.json({ message: "Cabin not found" });
+  }
+}
