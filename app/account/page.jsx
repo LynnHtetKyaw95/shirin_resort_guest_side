ddbcac1a011f87fadea924518580898c9ac34198
lhk <dklin1995@gmail.com>
2026-03-27 11:04:25 +0700

    login and logout

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,10 +1,16 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-const page = () => {
+const page = async () => {
+  const session = await auth();
+
   return (
-    <h2 className="font-semibold text-2xl text-primary mb-7">Welcome, Zeph</h2>
+    <h2 className="font-semibold text-2xl text-primary mb-7">
+      Welcome, <span className="text-accent-500">{session.user.name}</span>
+    </h2>
   );
 };
 export default page;
