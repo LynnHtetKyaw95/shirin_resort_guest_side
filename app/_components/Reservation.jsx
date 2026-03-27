dcd3dbad144869d97d346ed6f0eaeb7bfbfc48e1
lhk <dklin1995@gmail.com>
2026-03-27 09:29:38 +0700

    getting the user session

--- app/_components/Reservation.jsx
+++ app/_components/Reservation.jsx
@@ -1,6 +1,8 @@
 import DateSelector from "@/app/_components/DateSelector";
 import ReservationForm from "@/app/_components/ReservationForm";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/apiService";
+import { auth } from "../_lib/auth";
+import LoginMessage from "./LoginMessage";
 
 const Reservation = async ({ cabin }) => {
   const { id } = await cabin;
@@ -10,6 +12,8 @@ const Reservation = async ({ cabin }) => {
     getBookedDatesByCabinId(id),
   ]);
 
+  const session = await auth();
+
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-100">
       <DateSelector
@@ -17,7 +21,11 @@ const Reservation = async ({ cabin }) => {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 };
